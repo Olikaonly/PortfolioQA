3080c52f809f2d4cf54a4390b6f3e63493e4b6dd
Olga Shestakova <79090513+Olikaonly@users.noreply.github.com>
2022-02-16 17:29:08 +0700

    Create ImgurApiParams.java

---
+++ Автоматизированное тестирование REST API с использованием rest-assured/test/java/restApi/imgur/ImgurApiParams.java
@@ -0,0 +1,11 @@
+package restApi.imgur;
+
+/**
+ * Created by Olga Shestakova
+ * Date 09.11.2021
+ */
+public class ImgurApiParams {
+    public static String TOKEN = "*****";
+    public static String API_URL = "https://api.imgur.com";
+    public static String API_VERSION = "3";
+}
