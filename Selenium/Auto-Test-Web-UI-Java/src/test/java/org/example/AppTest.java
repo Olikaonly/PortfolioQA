d03010f18627c5282868d98953006d40387f30f0
Olga Shestakova <79090513+Olikaonly@users.noreply.github.com>
2022-02-16 15:03:53 +0700

    Create AppTest.java

---
+++ Selenium/Auto-Test-Web-UI-Java/src/test/java/org/example/AppTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("Test1");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2() {
+        System.out.println("Test2");
+    }
+}
