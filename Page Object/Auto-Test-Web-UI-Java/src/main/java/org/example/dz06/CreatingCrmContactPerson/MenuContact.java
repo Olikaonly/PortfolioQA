04c4cb0fb0f8cd223aa0711b28584f0b77027123
Olga Shestakova <79090513+Olikaonly@users.noreply.github.com>
2022-02-16 15:01:36 +0700

    Create MenuContact.java

---
+++ Page Object/Auto-Test-Web-UI-Java/src/main/java/org/example/dz06/CreatingCrmContactPerson/MenuContact.java
@@ -0,0 +1,29 @@
+package org.example.dz06.CreatingCrmContactPerson;
+
+import org.example.dz06.BaseView;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+/**
+ * Created by Olga Shestakova
+ * Date 06.09.2021
+ */
+public class MenuContact extends BaseView {
+    public MenuContact(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//span[text()='Контактные лица']")
+    public WebElement menuContactItem;
+
+    public void goMenuContact() {
+
+        menuContactItem.click();
+
+        webDriverWait.until(
+                ExpectedConditions.presenceOfElementLocated(By.xpath(CreateContact.createXpath)));
+    }
+}
