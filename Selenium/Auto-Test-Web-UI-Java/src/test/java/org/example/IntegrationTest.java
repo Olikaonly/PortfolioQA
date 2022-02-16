ef4da17cb3a73e8a854c76806325d4075ea3a34c
Olga Shestakova <79090513+Olikaonly@users.noreply.github.com>
2022-02-16 15:03:56 +0700

    Create IntegrationTest.java

---
+++ Selenium/Auto-Test-Web-UI-Java/src/test/java/org/example/IntegrationTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.Test;
+
+/**
+ * Created by Olga Shestakova
+ * Date 19.08.2021
+ */
+public class IntegrationTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("IntegrationTest");
+    }
+
+    @Test
+    public void shouldAnswerWithTrue2() {
+        System.out.println("IntegrationTest2");
+    }
+}
