c35b11770c8bac3fdf2943eb25c60b54c8593156
Olga Shestakova <79090513+Olikaonly@users.noreply.github.com>
2022-02-16 15:00:48 +0700

    Create AuthorizationSiteFlampTest.java

---
+++ Allure, протоколирование/Auto-Test-Web-UI-Java/src/test/java/org/example/dz06/AuthorizationSiteFlampTest/AuthorizationSiteFlampTest.java
@@ -0,0 +1,34 @@
+package org.example.dz06.AuthorizationSiteFlampTest;
+
+import io.qameta.allure.Description;
+import org.example.dz06.AuthorizationSiteFlamp.LoginPageFlamp;
+import org.example.dz06.BaseTest;
+import org.example.dz06.NavigationMenu;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static ru.yandex.qatools.htmlelements.matchers.WebElementMatchers.isDisplayed;
+
+/**
+ * Created by Olga Shestakova
+ * Date 06.09.2021
+ */
+public class AuthorizationSiteFlampTest extends BaseTest {
+    private NavigationMenu navigationMenu;
+
+    @Test
+    @Description("Тест авторизации на сайте")
+    void loginInCreateFlampTest() throws InterruptedException {
+        driver.get("https://novosibirsk.flamp.ru/");
+        new LoginPageFlamp(driver)
+                .clickLoginButtonTheSite()
+                .inputLogin("eolgis@ngs.ru")
+                .inputPasswordFlamp("qwerty123456")
+                .clickLoginButtonFlamp();
+
+        Thread.sleep(5000);
+
+        assertThat(new LoginPageFlamp(driver).saveElementFlamp, isDisplayed());
+
+    }
+}
